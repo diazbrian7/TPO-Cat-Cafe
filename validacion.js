fb820476acdde77f12aa441e41d1ba12af2cc4f2
Feli2002 <70671427+Feli2002@users.noreply.github.com>
2022-10-16 18:54:09 -0300

    Create validacion.js

---
+++ validacion.js
@@ -0,0 +1,32 @@
+(function(){
+    var formulario = document.getElementsByName('formulario')[0]
+    elementos = formulario.elements
+    boton = document.getElementById('btn')
+    
+    var validarNombre=function(e){
+      if(formulario.name.value == 0){
+        alert("Completa el nombre")
+        e.preventDefault()
+      }
+    }
+    var validarPersonas=function(e){
+      if(formulario.personas.value<=0){
+        alert("Tiene que haber una persona minimo")
+        e.preventDefault()
+      }         
+    }
+    var validarPersonas2 =function(e){
+      if(formulario.personas.value > 6){
+        alert("No pueden ser mas de 6 personas por mesa")
+        e.preventDefault()
+      }
+    }
+    
+    var validar = function(e){
+      validarNombre(e)
+      validarPersonas(e)
+      validarPersonas2(e)
+    }
+    formulario.addEventListener("submit",validar)
+
+  }())
